9e0da01b3fcd761ba5855d50f8d846e0df28e168
chenhb23 <chenhongbo_1994@163.com>
2026-01-21 09:01:11 +0800

    feat: 引入 vue-request

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import './assets/main.css'
 import 'element-plus/dist/index.css'
 
 import {createApp} from 'vue'
+import {setGlobalOptions} from 'vue-request'
 import {createPinia} from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
+
+setGlobalOptions({
+  manual: true,
+  // loadingDelay: 500,
+  // pagination: {
+  //   currentKey: 'pageNum',
+  //   pageSizeKey: 'pageSize',
+  //   totalKey: 'data.total',
+  // },
+})
 
 const pinia = createPinia()
 const app = createApp(App)
--- src/service/service.ts
+++ src/service/service.ts
@@ -1,11 +1,23 @@
 import axios from 'axios'
 import {enhance} from 'foca-axios'
 import {baseUrl} from '@/common/config'
+import {useUserStore} from '@/stores/user'
 
-const http = axios.create({
+const instance = axios.create({
   baseURL: baseUrl,
 })
 
-export default enhance(http, {
+const http = enhance(instance, {
   getHttpStatus: (response: any) => response.data?.code,
 })
+
+http.interceptors.request.use(config => {
+  config.headers['token'] = useUserStore().token
+  return config
+})
+
+http.interceptors.response.use(response => {
+  return response
+})
+
+export default http
