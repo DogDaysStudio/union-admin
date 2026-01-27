d54444327f91608e44ffdf5e210b7403424adb8a
chenhb23 <chenhongbo_1994@163.com>
2026-01-27 09:56:25 +0800

    feat: useTabStore改为对象的形式

--- src/stores/tabs.ts
+++ src/stores/tabs.ts
@@ -1,40 +1,32 @@
 import {defineStore} from 'pinia'
-import {ref} from 'vue'
 import type {RouteLocationNormalized} from 'vue-router'
 
 type TabRoute = Pick<RouteLocationNormalized, 'path' | 'meta' | 'matched'>
 type Tab = Pick<RouteLocationNormalized, 'path' | 'meta'> & {componentName: string}
 
-// todo: 改为对象的形式
-export const useTabStore = defineStore(
-  'tab',
-  () => {
-    const tabs = ref<Tab[]>([])
-
-    function addTab(tab: TabRoute) {
-      if (tabs.value.some(item => item.path === tab.path)) {
+export const useTabStore = defineStore('tab', {
+  state: () => ({
+    tabs: [] as Tab[],
+  }),
+  actions: {
+    addTab(tab: TabRoute) {
+      if (this.tabs.some(item => item.path === tab.path)) {
         return
       }
 
-      tabs.value.push({
+      this.tabs.push({
         path: tab.path,
         meta: tab.meta,
         componentName: getComponentName(tab),
       })
-    }
+    },
+    removeTab(path: string) {
+      this.tabs = this.tabs.filter(item => item.path !== path)
+    },
+  },
+  // persist: true,
+})
 
-    function removeTab(path: string) {
-      tabs.value = tabs.value.filter(item => item.path !== path)
-    }
-
-    return {tabs, addTab, removeTab}
-  }
-  // {
-  //   persist: true,
-  // }
-)
-
-// todo: fix: 获取不到 name 会返回 component
 export function getComponentName(route: TabRoute) {
   return route.matched?.[route.matched?.length - 1]?.components?.default?.name || ''
 }
