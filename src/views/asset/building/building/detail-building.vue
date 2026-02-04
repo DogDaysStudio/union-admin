6f45982b6fc93b81fd6719706b835b83fc3a5081
caiyicheng <caiyicheng@anjueasy.com>
2026-02-04 15:54:33 +0800

    feat(asset): 优化楼栋详情代码

--- src/views/asset/building/building/detail-building.vue
+++ src/views/asset/building/building/detail-building.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import {reactive, onMounted} from 'vue'
-import type {FormRules} from 'element-plus'
 import {useRouter, useRoute} from 'vue-router'
 import {amsAsset} from '@/service/api/amsAsset'
 import {useRequest} from 'vue-request'
@@ -8,19 +7,11 @@ import {useRequest} from 'vue-request'
 const router = useRouter()
 const route = useRoute()
 
-// 楼栋详情
 const buildingGet = useRequest(amsAsset.amsAssetBuildingGet, {
   throttleInterval: 500,
 })
 
-// 初始化表单数据：响应式对象，与表单双向绑定
 const formData = reactive({} as AssetBuildingVO)
-
-// 表单验证规则：对应prop字段，实现必填/格式校验
-const formRules = reactive<FormRules>({
-  buildingName: {required: true, message: '请填写楼栋名称', trigger: 'blur'},
-  ownershipUnitCode: {required: true, message: '请选择产权单位', trigger: 'change'},
-})
 
 onMounted(() => getDetail())
 
@@ -45,13 +36,7 @@ const handleSubmit = () => router.push('/asset/management/building-floor')
       </div>
     </template>
     <div class="mx-auto">
-      <el-form
-        :model="formData"
-        :rules="formRules"
-        ref="formRef"
-        label-width="80px"
-        label-position="top"
-      >
+      <el-form :model="formData" ref="formRef" label-width="80px" label-position="top">
         <el-row :gutter="24">
           <el-col :span="8">
             <el-form-item label="楼栋名称" required>
